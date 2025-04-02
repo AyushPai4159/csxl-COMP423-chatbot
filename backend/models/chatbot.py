f4ee410cc57ce61bc4c064ad4f887ec42927757f
ayushTheunc <ayushpai@unc.edu>
2025-04-02 17:30:50 -0400

    combined ChatMessage and ChatSession into one file

--- backend/models/chatbot.py
+++ backend/models/chatbot.py
@@ -1,9 +1,18 @@
 from pydantic import BaseModel, Field
 from public_user import PublicUser
-from message_response import ChatMessageResponse
+
+
+
+class ChatMessageResponse(BaseModel):
+    Chat_id: int
+    User_prompt: str
+    Api_response: str
 
 class ChatSession(BaseModel):
     Session_Id: int
     Onyen: str
     User: PublicUser
     User_chats: list[ChatMessageResponse] # Each Tuple is a user prompt followed 
+
+
+
--- backend/models/message_response.py
+++
@@ -1,8 +0,0 @@
-
-from pydantic import BaseModel, Field
-
-
-class ChatMessageResponse(BaseModel):
-    Chat_id: int
-    User_prompt: str
-    Api_response: str
