cb3fe5e1ff74eae6d5c638cc78eb71548bf536c6
Daniel Zhang <dzahng@unc.edu>
2025-04-02 15:25:25 -0400

    added chatbot.py routing, outlined the routes
    l

---
+++ backend/api/chatbot.py
@@ -0,0 +1,55 @@
+"""Chatbot API
+
+Chatbot routes are used to interact with the chatbot
+"""
+
+from fastapi import APIRouter, Depends
+
+from typing import List
+
+openapi_tags = {
+    "name": "Chatbot",
+    "description": "Chat, view, update, and delete chat logs and history.",
+}
+
+api = APIRouter(prefix="/api/chatbot")
+
+
+@api.post("/chat", tags=["Chatbot"])
+def chat(user_message: str) -> str:
+    """
+    Send a message to the chatbot api and receive a response
+    """
+    return ""
+
+
+@api.get("/admin/session/{session_id}", tags=["Chatbot"])
+def get_session_history(session_id: int) -> ChatSession:
+    """
+    Get the chat session history for a given session id
+    """
+    return None
+
+
+@api.patch("/admin/session/{session_id}", tags=["Chatbot"])
+def update_session_history(session_id: int, chat_session: ChatSession):
+    """
+    Update the chat session history for a given session id
+    """
+    return None
+
+
+@api.patch("/admin/session/{session_id}/{chat_index}", tags=["Chatbot"])
+def update_chat_message(session_id: int, chat_index: int, chat_message: ChatMessage):
+    """
+    Update a specific chat message in a chat session history
+    """
+    return None
+
+
+@api.delete("/admin/session/{session_id}", tags=["Chatbot"])
+def delete_session(session_id: int):
+    """
+    Delete a chat session history
+    """
+    return None
