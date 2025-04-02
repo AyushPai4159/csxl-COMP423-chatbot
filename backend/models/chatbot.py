6e4fa02337f2cf7a8d913cc171f5e243e6f42271
Daniel Zhang <dzahng@unc.edu>
2025-04-02 17:37:09 -0400

    models before merge

--- backend/models/chatbot.py
+++ backend/models/chatbot.py
@@ -2,17 +2,14 @@ from pydantic import BaseModel, Field
 from public_user import PublicUser
 
 
-
 class ChatMessageResponse(BaseModel):
     Chat_id: int
     User_prompt: str
     Api_response: str
 
+
 class ChatSession(BaseModel):
     Session_Id: int
     Onyen: str
     User: PublicUser
-    User_chats: list[ChatMessageResponse] # Each Tuple is a user prompt followed 
-
-
-
+    User_chats: list[ChatMessageResponse]  # Each Tuple is a user prompt followed
