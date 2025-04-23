c3821dfdfba472f378e92c5c104061a97bd462f5
Mann Barot <62913683+MannBarot@users.noreply.github.com>
2025-04-23 13:26:18 -0400

    New chatbox look (#39)
    * committing from switching to frontend2
    
    * major changes to chatbot frontend

--- frontend/src/app/chatbot/chatbot.component.ts
+++ frontend/src/app/chatbot/chatbot.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -9,14 +9,13 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './chatbot.component.css'
 })
 export class ChatbotComponent {
+  @ViewChild('messagesContainer') private messagesContainer!: ElementRef;
   constructor(private http: HttpClient) {}
 
   isChatbotVisible = false;
 
   messages = [
     { sender: 'bot', text: 'Good evening [name], how can I help you today?' },
-    { sender: 'user', text: 'What events are happening this week?' },
-    { sender: 'bot', text: 'We’ve got a Hackathon on Friday! 🎉' }
   ];
 
   newMessage: string = '';
@@ -33,6 +32,7 @@ export class ChatbotComponent {
     // If user message is empty, do not send
     if (userMessage.length > 0) {
       this.messages.push({ sender: 'user', text: userMessage });
+      this.newMessage = ''; //clears the input field
       this.http
         .post<any>('https://csxl-team-e3-comp423-25s.apps.unc.edu/welcome/api/chatbot/admin/chat', {
           chat_id: 0,
@@ -44,6 +44,7 @@ export class ChatbotComponent {
           (response: string) => {
             this.messages.push({ sender: 'bot', text: response });
             this.newMessage = '';
+            this.scrollToBottom();
           },
           (error) => {
             console.error('Error:', error);
@@ -51,8 +52,26 @@ export class ChatbotComponent {
               sender: 'bot',
               text: "Sorry, I couldn't process your request."
             });
+            this.scrollToBottom();
           }
         );
+        setTimeout(() => this.scrollToBottom(),1000);
     }
+  }
+
+  scrollToBottom(){
+    try {
+      this.messagesContainer.nativeElement.scrollTop = this.messagesContainer.nativeElement.scrollHeight;
+    } catch (err) {
+      console.warn('Could not scroll to bottom:', err);
+    }
+  }
+
+  resetSession(){
+    //saves current session and resets chat window
+  }
+
+  showSessions(){
+    //shows previous user sessions with the bot
   }
 }
